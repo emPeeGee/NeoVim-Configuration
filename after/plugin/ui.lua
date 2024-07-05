6d96b66a8d9125aadeee16c484c277749c571b9d
emPeeGee <eempeegee@gmail.com>
2024-07-05 22:14:10 +0300

    feat: itl setup for 0.10

---
+++ after/plugin/ui.lua
@@ -0,0 +1,96 @@
+local color = require("color")
+
+require("battery").setup({})
+require("satellite").setup({
+  current_only = false,
+  winblend = 50,
+  zindex = 40,
+  excluded_filetypes = {},
+  width = 2,
+  handlers = {
+    search = {
+      enable = true,
+    },
+    diagnostic = {
+      enable = true,
+    },
+    gitsigns = {
+      enable = true,
+    },
+    marks = {
+      enable = true,
+      show_builtins = false, -- shows the builtin marks like [ ] < >
+    },
+  },
+})
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
+local battery = function()
+  return require("battery").get_status_line()
+end
+
+local time = function()
+  return os.date("%H:%M")
+end
+
+require("lualine").setup({
+  options = {
+    globalstatus = true,
+  },
+  extensions = { "nvim-tree" },
+  sections = {
+    --lualine_b = {'branch', 'diff', 'diagnostics'},
+    --FIX: is not working https://github.com/nvim-lualine/lualine.nvim/issues/799
+    lualine_b = { "branch", { "diff", source = diff_source }, "diagnostics" },
+    lualine_x = { battery },
+    lualine_z = { time },
+  },
+})
+
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup({ scope = { highlight = highlight } })
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+require("bufferline").setup({
+  options = {
+    -- diagnostics = "coc",
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
+  },
+})
