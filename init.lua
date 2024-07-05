d8f601cd9c21d0d0a323fcd1969df193e270bd40
emPeeGee <eempeegee@gmail.com>
2024-07-05 22:51:40 +0300

    chore: remove trouble

--- init.lua
+++ init.lua
@@ -44,17 +44,7 @@ ccc.setup({
   },
 })
 
--- TODO: config
-require("trouble").setup({
-  position = "bottom", -- position of the list can be: bottom, top, left, right
-  height = 8, -- height of the trouble list when position is top or bottom
-  icons = true, -- use devicons for filenames
-  auto_open = true, -- automatically open the list when you have diagnostics
-  auto_close = true, -- automatically close the list when you have no diagnostics
-  mode = "quickfix",
-})
-
--- require("better_escape").setup()
+require("better_escape").setup()
 require("treesj").setup({ use_default_keymaps = false, max_join_length = 240 })
 vim.keymap.set("n", "gJ", ":TSJToggle<CR>")
 
