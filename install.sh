8dd78cf14c994781315317526253345b4fc438bd
emPeeGee <eempeegee@gmail.com>
2024-11-20 21:00:05 +0200

    ✨ feat: move tmux file

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+ln -sf ~/.dotfiles/nvim ~/.config
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+# ln -s ~/.dotfiles/alacritty ~/.config
+# ln -s ~/.dotfiles/sketchybar/ ~/.config
+# ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc 
+# ln -s ~/.dotfiles/tmux/catppuccin-everforest.tmuxtheme ~/.tmux/plugins/tmux/catppuccin-everforest.tmuxtheme  
